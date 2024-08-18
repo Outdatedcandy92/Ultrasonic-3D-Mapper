5a8a741b8111fd7de62f85ba6d652acbdfa13022
Outdatedcandy92 <138517406+Outdatedcandy92@users.noreply.github.com>
2024-08-17 21:21:22 -0400

    interpolation

--- python/main.py
+++ python/main.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-import math
 from scipy.interpolate import griddata
+import math
 
 
 x_data = []
@@ -13,11 +13,11 @@ z_data = []
 with open('data.txt', 'r') as file:
     for line in file:
         z, x, y = map(int, line.strip().split(','))
-        z_data.append(-z)
+        z = (math.sin(90-z)) * y
+        z_data.append(z)
         x_data.append(x)
         y = (math.cos(z)) * y
         y_data.append(y)
-
 
 
 x_data = np.array(x_data)
@@ -30,11 +30,22 @@ print("Y Data:", y_data)
 print("Z Data:", z_data)
 
 
+print("Z Data Range:", np.min(z_data), np.max(z_data))
+
+grid_x, grid_y = np.mgrid[min(x_data):max(x_data):100j, min(y_data):max(y_data):100j]
+
+
+grid_z = griddata((x_data, y_data), z_data, (grid_x, grid_y), method='nearest')
+
+
+print("Interpolated Z Data Range:", np.min(grid_z), np.max(grid_z))
+
+
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
 
-ax.scatter(x_data, y_data, z_data, c='r', marker='o')
+ax.plot_surface(grid_x, grid_y, grid_z, cmap='viridis')
 
 
 ax.set_xlabel('X Label')
